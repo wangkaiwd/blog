48eedc6bf9261a1ca57208deafd2c8483fb8434d
wangkaiwd <1162486176@qq.com>
2019-02-10 19:14:55 +0800

    feat: 根据请求路径来读取相应目录的响应文件

--- app/index.js
+++ app/index.js
@@ -4,11 +4,18 @@ const fs = require('fs');
 class App {
   initServer () {
     return (req, res) => {
-      fs.readFile('./package.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.write(data);
-        res.end();
-      });
+      const { url } = req;
+      const staticFunc = (url) => {
+        if (url === '/favicon.ico') return;
+        if (url === '/') {
+          url = '/index.html';
+        }
+        fs.readFile(`./public${url}`, 'utf8', (err, data) => {
+          if (err) throw err;
+          res.end(data);
+        });
+      };
+      staticFunc(url);
     };
   }
 }
