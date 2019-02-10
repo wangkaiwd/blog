f89c3c79ba6116f326124531af4aa6ca3e2df769
wangkaiwd <1162486176@qq.com>
2019-02-10 22:46:29 +0800

    feat: 通过a标签实现页面跳转

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ const staticFunc = require('./staticServer');
 class App {
   initServer () {
     return (req, res) => {
+      // 例： url = /css/reset.css,这里的'/'代表服务器根路径: localhost:3000
       const { url } = req;
       const body = staticFunc(url);
       res.end(body);
--- app/staticServer/index.js
+++ app/staticServer/index.js
@@ -7,14 +7,17 @@ const fs = require('fs');
 const getPath = dir => path.resolve(process.cwd(), `public${dir}`);
 const staticFunc = (url) => {
   if (url === '/favicon.ico') return;
-  if (url === '/') {
-    url = '/index.html';
-  }
+  const html = {
+    '/': '/index.html',
+    '/about': '/about.html',
+    '/list': '/list.html'
+  };
   // 这里的路径是相对于process.cwd(): 当前进程执行目录来进行获取的
   // 即相对于app/index.js
   let body;
+  const path = getPath(html[url] || url);
   try {
-    body = fs.readFileSync(getPath(url));
+    body = fs.readFileSync(path);
   } catch (e) {
     body = `NOT FOUND ${e.message}`;
   }
