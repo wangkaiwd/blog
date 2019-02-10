cec1bacc946303dcbee5f3faa88cb44ea9faa24b
wangkaiwd <1162486176@qq.com>
2019-02-10 19:41:05 +0800

    feat: 优化路径获取方法并添加reset.css

--- app/index.js
+++ app/index.js
@@ -1,16 +1,20 @@
 // 主要核心逻辑入口
 const fs = require('fs');
+const path = require('path');
 
 class App {
   initServer () {
     return (req, res) => {
       const { url } = req;
+      const getPath = dir => path.resolve(process.cwd(), `public${dir}`);
       const staticFunc = (url) => {
         if (url === '/favicon.ico') return;
         if (url === '/') {
           url = '/index.html';
         }
-        fs.readFile(`./public${url}`, 'utf8', (err, data) => {
+        // 这里的路径是相对于process.cwd(): 当前进程执行目录来进行获取的
+        // 即相对于app/index.js
+        fs.readFile(getPath(url), (err, data) => {
           if (err) throw err;
           res.end(data);
         });
