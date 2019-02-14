a80126d99004be35f95f11250acffc76a77f3910
wangkaiwd <1162486176@qq.com>
2019-02-14 23:52:30 +0800

    feat: Promise.then方法返回值5中情况中的前4种学习

--- app/api/cb-promise.js
+++ app/api/cb-promise.js
@@ -10,13 +10,38 @@ const timeout = (ms) => {
      *    3. param1,...,paramN: 可选，附加参数，一旦定时器到期，它们会作为参数传递给function
      */
     setTimeout(resolve, ms, 'done');
+    // return 的内容将会被忽略
+    return 'I am return value';
   });
 };
+// then方法返回一个Promise,而它的行为与then中的回调函数的返回值有关：
 timeout(100).then(result => {
   console.log(result);
-});
+  // 代码报错或者执行Promise.reject才会执行下一个.then函数的错误回调
+  //    1. 如果then中的回调函数返回一个值，那么then返回的Promise将会成为接受状态，并且将返回的值作为接受状态的回调函数的参数
+  // return 1
+  //    2. 如果then中的回调函数抛出一个错误，那么then返回的Promise将会成为拒绝状态，并且将抛出的错误作为拒绝状态的回调函数的参数
+  // const y = x + 1
+  //    3. 如果then中的回调函数返回的是一个已经接受状态的Promise,那么then返回的Promise也会成为接受状态，并且将那个Promise
+  //       的接受状态的回调函数的参数值作为该被返回的Promise的接受状态回调函数的参数值
+  return Promise.resolve(1);
+  //  Promise.resolve(1) ====>> new Promise((resolve) => resolve(1))
+  //    4. 拒绝状态同3，如果then中的回调函数返回的是一个已经拒绝状态的Promise,那么then返回的Promise也会成为拒绝状态，并且将那个Promise
+  //       的拒绝状态的回调函数的参数值作为该被返回的Promise的拒绝状态回调函数的参数值
+  // return Promise.reject(1);
+  //  Promise.reject(1) ====>> new Promise((resolve,reject) => reject(1))
+  //    5.
+}).then(
+  result2 => {
+    console.log(result2);
+  },
+  err => {
+    console.log('err2', err);
+  }
+);
 console.log('Hi');
 // Promise新建后就会立即执行
 // 'Promise'
 // 'Hi'
 // 'done'
+
